5c89adbbbd9fbb6b77834d1acab94f56eb29b923
irof <irof.ocean@gmail.com>
2019-08-23 02:33:09 +0900

    ビルドしたjarが動かせる

--- src/main/java/perfunctory/secretary/Application.java
+++ src/main/java/perfunctory/secretary/Application.java
@@ -5,9 +5,9 @@ package perfunctory.secretary;
 
 import io.micronaut.runtime.Micronaut;
 
-public class App {
+public class Application {
 
     public static void main(String[] args) {
-        Micronaut.run(App.class);
+        Micronaut.run(Application.class);
     }
 }
